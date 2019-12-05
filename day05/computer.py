8c2e126d43f565cb57905576723ca83fe6b94b79
Doga Tekin <doga.tekin@epfl.ch>
2019-12-05 23:14:47 +0000

    Add day5

---
+++ day05/computer.py
@@ -0,0 +1,59 @@
+def get(xs, i, mode):
+    if mode == '0':
+        return xs[xs[i]]
+    elif mode == '1':
+        return xs[i]
+
+def compute(xs):
+    i = 0
+    ins = xs[i]
+    op = str(ins)[-2:]
+    modes = str(ins)[-3::-1]
+    modes = modes + '0'*(2-len(modes))
+    
+    while op != '99':
+        if op.endswith('1'):
+            xs[xs[i+3]] = get(xs, i+1, modes[0]) + get(xs, i+2, modes[1])
+            i += 4
+        elif op.endswith('2'):
+            xs[xs[i+3]] = get(xs, i+1, modes[0]) * get(xs, i+2, modes[1])
+            i += 4
+        elif op.endswith('3'):
+            xs[xs[i+1]] = int(input('Input ID: '))
+            i += 2
+        elif op.endswith('4'):
+            print(get(xs, i+1, modes[0]))
+            i += 2
+        elif op.endswith('5'):
+            if get(xs, i+1, modes[0]):
+                i = get(xs, i+2, modes[1])
+            else:
+                i += 3
+        elif op.endswith('6'):
+            if not get(xs, i+1, modes[0]):
+                i = get(xs, i+2, modes[1])
+            else:
+                i += 3
+        elif op.endswith('7'):
+            if get(xs, i+1, modes[0]) < get(xs, i+2, modes[1]):
+                xs[xs[i+3]] = 1
+            else:
+                xs[xs[i+3]] = 0
+            i += 4
+        elif op.endswith('8'):
+            if get(xs, i+1, modes[0]) == get(xs, i+2, modes[1]):
+                xs[xs[i+3]] = 1
+            else:
+                xs[xs[i+3]] = 0
+            i += 4
+        else:
+            raise IndexError
+        
+        ins = xs[i]
+        op = str(ins)[-2:]
+        modes = str(ins)[-3::-1]
+        modes = modes + '0'*(2-len(modes))
+    
+    return xs
+
+assert compute([1002,4,3,4,33]) == [1002,4,3,4,99]
