c773fcd406513184e37070c9cb32d151234113aa
Doga Tekin <doga.tekin@epfl.ch>
2019-12-24 16:35:27 +0300

    Add day15

---
+++ day15/part1.py
@@ -0,0 +1,51 @@
+from computer import compute
+
+def print_area(explored):
+    min_x = (min(explored.keys(), key=lambda pos: pos[0]))[0]
+    min_y = (min(explored.keys(), key=lambda pos: pos[1]))[1]
+    
+    max_x = (max(explored.keys(), key=lambda pos: pos[0]))[0]
+    max_y = (max(explored.keys(), key=lambda pos: pos[1]))[1]
+    
+    for y in range(min_y, max_y+1):
+        for x in range(min_x, max_x+1):
+            if (x, max_y-y+min_y) in explored:
+                print(explored[(x, max_y-y+min_y)], end='')
+            else:
+                print(' ', end='')
+        print()
+                
+
+def explore(xs, d, ip, rel_base, explored, pos, step):
+    out, new_xs, new_ip, new_rel_base = compute(xs, d, ip, rel_base)
+    
+    x, y = pos
+    dx, dy = ((0, 1), (0, -1), (-1, 0), (1, 0))[d-1]
+    new_pos = (x+dx, y+dy)
+    
+    if new_pos in explored:
+        return
+    
+    if out == 0:
+        explored[new_pos] = '#'
+    else:
+        if out == 1:
+            explored[new_pos] = '.'
+        else:
+            explored[new_pos] = '@'
+            print(step)
+        
+        for val in range(1, 5):
+            explore(new_xs, val, new_ip, new_rel_base, explored, new_pos, step+1)
+
+
+with open('input.txt') as f:
+    xs = [int(x) for x in f.read().split(',')] + [0]*1000
+ 
+explored = {(0, 0): 'D'}
+ip = rel_base = 0
+
+for d in range(1, 5):
+    explore(xs, d, ip, rel_base, explored, (0, 0), 1)
+
+# print_area(explored)
